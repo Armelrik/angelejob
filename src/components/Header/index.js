afcf7d516d7f70826f0ddf25a7913cbf3db2334d
armelrik <armelrik@hotmail.com>
2023-08-09 18:10:30 +0000

    changes:09/08/23

---
+++ src/components/Header/index.js
@@ -0,0 +1,19 @@
+import { Box, Grid, Typography, Button } from "@mui/material";
+import React from "react";
+
+const Header = (props) => (
+    <Box px={10} py={10} bgcolor="secondary.second" color="white">
+        <Grid container justify="center">
+            <Grid item xs={10} >
+                <Box display="flex" justifyContent="space-between">
+                    <Typography variant="h4">Liste des offres disponible</Typography>
+                    <Button variant="contained" color="secondary" disableElevation>Poster un job</Button>
+            </Box>
+            </Grid>
+            
+        </Grid>
+        
+    </Box>
+);
+
+export default Header;
